9f9d20df355316ff0d01b00229403c0614893d11
Toma Naujokaite <toma.naujokaite@Tomas-MacBook-Pro.local>
2022-03-04 10:49:43 +0200

    routes

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -8,12 +8,31 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
-tasks: Task[]= TASKS;
+tasks: Task[]= [];
 
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=> this.tasks = tasks);
   }
+
+deleteTask(task: Task){
+  this.taskService.deleteTask(task).subscribe(()=>(this.tasks = this.tasks.filter(t=> t.id !== task.id)))
+}
+
+toggleReminder(task: Task){
+  task.reminder = !task.reminder;
+ this.taskService.updateTaskReminder(task).subscribe();
+}
+
+addTask(task: Task){
+  this.taskService.addTask(task).subscribe((task)=> this.tasks.push(
+    task
+  ));
+  
+}
 
 }
